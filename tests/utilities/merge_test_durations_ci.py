0547f47efdd763aef97307448c08e28a712400f3
Claudio Usai <c.usai186@gmail.com>
2021-12-20 15:03:20 +0100

    Distinguish python version
    Signed-off-by: Claudio Usai <c.usai186@gmail.com>

--- tests/utilities/merge_test_durations_ci.py
+++ tests/utilities/merge_test_durations_ci.py
@@ -1,19 +1,20 @@
 import json
-import argparse
+import re
 from pathlib import Path
 
 
-def merge_durations(filename: str, prefix: str):
-    split_paths = Path(".").glob(f"{prefix}-*/{filename}")
-    durations_path = Path(filename)
+def merge_durations(files: list, output_suffix: str):
+    outname = f"test_durations/.test_durations_{output_suffix}"
+    durations_path = Path(outname)
     try:
         previous_durations = json.loads(durations_path.read_text())
     except FileNotFoundError:
         previous_durations = {}
     new_durations = previous_durations.copy()
 
-    for path in split_paths:
-        durations = json.loads(path.read_text())
+    for path in files:
+        d = Path(path)
+        durations = json.loads(d.read_text())
         new_durations.update(
             {
                 name: duration
@@ -22,15 +23,21 @@ def merge_durations(filename: str, prefix: str):
             }
         )
 
-    with open(filename, 'w') as f:
+
+    durations_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(outname, 'w') as f:
         f.writelines(json.dumps(new_durations))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-d", "--durations-path", type=str, help="Path of duration files")
-    parser.add_argument(
-        "-p", "--prefix", type=str, help="File prefix")
-    args = parser.parse_args()
-    merge_durations(args.durations_path, args.prefix)
+    regex = re.compile(r'.*python-((\d+\.?)+).*')
+    files = Path(".").glob("duration-chunk-*/*")
+    versions = {}
+    for f in files:
+        m = regex.match(str(f))
+        if m:
+            versions[m.group(1)] = [m.group(0)] + versions.get(m.group(1), [])
+
+    for py_vers, file_list in versions.items():
+        merge_durations(file_list, py_vers)
